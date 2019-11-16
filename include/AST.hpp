aa9ba33e2ed069857deedd71183f6e8beda78c4f
Willem Melis <willem.melis@outlook.com>
2019-11-16 19:53:37 +0100

    Added std:variant AST
    * usng a fixed-point combinator to get the recursive stuff working.

--- include/AST.hpp
+++ include/AST.hpp
@@ -1,68 +1,78 @@
-/// ExprAST - Base class for all expression nodes.
-class ExprAST {
-public:
-  virtual ~ExprAST() {}
-};
+#include<variant>
+#include<string>
+#include<memory>
+#include<vector>
 
 /// NumberExprAST - Expression class for numeric literals like "1.0".
-class NumberExprAST : public ExprAST {
-  double Val;
+struct NumberExprAST {
+  const double val;
 
-public:
-  NumberExprAST(double Val) : Val(Val) {}
+  NumberExprAST(double val) : val(val) {}
 };
 
 /// VariableExprAST - Expression class for referencing a variable, like "a".
-class VariableExprAST : public ExprAST {
-  std::string Name;
+struct VariableExprAST {
+  const std::string name;
 
-public:
-  VariableExprAST(const std::string &Name) : Name(Name) {}
+  VariableExprAST(const std::string &name) : name(name) {}
 };
 
 /// BinaryExprAST - Expression class for a binary operator.
-class BinaryExprAST : public ExprAST {
-  char Op;
-  std::unique_ptr<ExprAST> LHS, RHS;
+template<typename T>
+struct BinaryExprAST {
+  const char op;
+  const std::unique_ptr<T> LHS, RHS;
 
-public:
-  BinaryExprAST(char op, std::unique_ptr<ExprAST> LHS,
-                std::unique_ptr<ExprAST> RHS)
-    : Op(op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+  BinaryExprAST(char op, std::unique_ptr<T> LHS,
+                std::unique_ptr<T> RHS)
+    : op(op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
 };
 
 /// CallExprAST - Expression class for function calls.
-class CallExprAST : public ExprAST {
-  std::string Callee;
-  std::vector<std::unique_ptr<ExprAST>> Args;
+template<typename T>
+struct CallExprAST {
+  const std::string callee;
+  const std::vector<std::unique_ptr<T>> args;
 
-public:
-  CallExprAST(const std::string &Callee,
-              std::vector<std::unique_ptr<ExprAST>> Args)
-    : Callee(Callee), Args(std::move(Args)) {}
+  CallExprAST(const std::string &callee,
+              std::vector<std::unique_ptr<T>> args)
+    : callee(callee), args(std::move(args)) {}
 };
 
-/// PrototypeAST - This class represents the "prototype" for a function,
-/// which captures its name, and its argument names (thus implicitly the number
-/// of arguments the function takes).
-class PrototypeAST {
-  std::string Name;
-  std::vector<std::string> Args;
+// -> recursively define the variant using fixed-point combinator
+template<typename T>
+using ExprASTNonRecursive = std::variant<
+    NumberExprAST,
+    VariableExprAST,
+    BinaryExprAST<T>,
+    CallExprAST<T>>;
 
-public:
-  PrototypeAST(const std::string &name, std::vector<std::string> Args)
-    : Name(name), Args(std::move(Args)) {}
-
-  const std::string &getName() const { return Name; }
+// tie the knot
+template<template<typename> typename K>
+struct ExprASTTemplated : K<ExprASTTemplated<K>>
+{
+    using K<ExprASTTemplated> :: K;
 };
 
-/// FunctionAST - This class represents a function definition itself.
-class FunctionAST {
-  std::unique_ptr<PrototypeAST> Proto;
-  std::unique_ptr<ExprAST> Body;
+using ExprAST = ExprASTTemplated<ExprASTNonRecursive>;
 
-public:
-  FunctionAST(std::unique_ptr<PrototypeAST> Proto,
-              std::unique_ptr<ExprAST> Body)
-    : Proto(std::move(Proto)), Body(std::move(Body)) {}
+// PrototypeAST - This class represents the "prototype" for a function,
+// which captures its name, and its argument names (thus implicitly the number
+// of arguments the function takes).
+struct PrototypeAST {
+  const std::string name;
+  const std::vector<std::string> args;
+
+  PrototypeAST(const std::string &name, std::vector<std::string> args)
+    : name(name), args(std::move(args)) {}
+};
+
+// FunctionAST - This class represents a function definition itself.
+struct FunctionAST {
+  const std::unique_ptr<PrototypeAST> proto;
+  const std::unique_ptr<ExprAST> body;
+
+  FunctionAST(std::unique_ptr<PrototypeAST> proto,
+              std::unique_ptr<ExprAST> body)
+    : proto(std::move(proto)), body(std::move(body)) {}
 };
